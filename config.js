5dcc74f344c84579dcbb74337978c1640332c59b
mnhtri <86742736+mnhtri@users.noreply.github.com>
2021-07-01 02:36:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'helu prin xừ 1tháng trước của mtri!',
     introDesc: `mtri chỉ  muốn nói mtri vẫn yêu qgiao 🥺`,
-    btnIntro: '=)))',
-    title: 'hoàng tử đã ổn chưa nè , hmmm mtri muốn nói là mình quay lại được hong🥺',
+    btnIntro: ...',
+    title: 'hoàng tử đã ổn chưa nè , hmmm mtri muốn nói là mình quay lại được hong🥺.Dù cũng đoán chắc 99.99% là k đc nma vẫn muốn làm vì muốn cho qgiao biết là mtri vẫn còn yêu qgiao...',
     desc: 'mình quay lại với nhau đượt hong 🥺 ? ',
-    btnYes: 'oki nè ',
+    btnYes: 'được nè ',
     btnNo: 'không quay lại đc nè ',
     question:'qgiao thấy k đc thì coi như đây là một web hay(maybe là z) mà mtri học đc trên tiktok cũng như là 1 cái trend tretrau mà mtri làm thôi ha ',
     btnReply: 'reply to mtri nè ',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'có gì làm xog nhắn tui nha',
+    mess: 'mtri đợi qgiao nè =))',
     messDesc: 'aukay dù sao thì cũng cảm ơn qgiao nhiều nhó .',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun =))',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
